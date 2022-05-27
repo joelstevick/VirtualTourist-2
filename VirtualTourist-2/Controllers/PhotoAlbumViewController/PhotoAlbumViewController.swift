9c9608afd4a8ef0ee6da275487bfda095ab40201
joel stevick <joelstevick@outlook.com>
2022-05-27 05:08:27 -0400

    restore map in album

--- VirtualTourist-2/Controllers/PhotoAlbumViewController/PhotoAlbumViewController.swift
+++ VirtualTourist-2/Controllers/PhotoAlbumViewController/PhotoAlbumViewController.swift
@@ -6,26 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoAlbumViewController: UIViewController {
 
     // MARK - Properties
     var location: Location!
     var dataController: DataController!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        restoreMap(mapView: mapView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
