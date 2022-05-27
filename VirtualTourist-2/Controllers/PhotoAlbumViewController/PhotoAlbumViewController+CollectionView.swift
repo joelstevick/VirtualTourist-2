36039f6f6920a51ba4d0cf39eefb6983a5b274df
joel stevick <joelstevick@outlook.com>
2022-05-27 09:39:57 -0400

    add some layout hooks

--- VirtualTourist-2/Controllers/PhotoAlbumViewController/PhotoAlbumViewController+CollectionView.swift
+++ VirtualTourist-2/Controllers/PhotoAlbumViewController/PhotoAlbumViewController+CollectionView.swift
@@ -34,6 +34,14 @@ extension PhotoAlbumViewController: UICollectionViewDelegate, UICollectionViewDa
         return CGSize(width: imageSideLen, height: imageSideLen)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-           return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        }
+        return UIEdgeInsets.zero
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
